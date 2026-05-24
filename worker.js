fde2ab443d4a42e4579ec1ff7d562a097748de78
shlexx <ginirbi@gmail.com>
2026-05-24 22:48:35 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -6,7 +6,8 @@ export default {
     for (const [key, value] of params) {
       if (key.trim().toLowerCase() === 'id') ids.push(value);
     }
-    const last = ids.at(-1)?.replace(/\+/g, '').replace(/^0+/, '') ?? "none found";
-    return new Response(last);
+    const last = ids.at(-1)?.replace(/\+/g, '').replace(/^0+/, '');
+    const final = last ? (isNaN(last) ? String(parseInt(last, 16)) : last) : "none found";
+    return new Response(final);
   }
 }
